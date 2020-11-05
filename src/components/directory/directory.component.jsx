2a16fcb39b1e4fa4aadd75c3da2a7fcccc12c4b5
dziwanski <anw93iw@gmail.com>
2020-11-05 17:23:35 +0100

    update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,28 +14,33 @@ class Directory extends Component {
 					id: 1,
 					title: 'hats',
 					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+					linkUrl: 'hats'
 				},
 				{
 					id: 2,
 					title: 'jackets',
 					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+					linkUrl: ''
 				},
 				{
 					id: 3,
 					title: 'sneakers',
 					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+					linkUrl: ''
 				},
 				{
 					id: 4,
 					title: 'womens',
 					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-					size: 'large'
+					size: 'large',
+					linkUrl: ''
 				},
 				{
 					id: 5,
 					title: 'mens',
 					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-					size: 'large'
+					size: 'large',
+					linkUrl: ''
 				},
 			]
 		}
@@ -43,12 +48,12 @@ class Directory extends Component {
 
 
 	render() {
-
+		console.log(this.props);
 		return (
 			<div className='directory-menu'>
 				{
-					this.state.sections.map(({ id, title, imageUrl, size }) => (
-						<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+					this.state.sections.map(({ id, ...otherPSectionProps }) => (
+						<MenuItem key={id} {...otherPSectionProps} />
 					))
 				}
 			</div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,10 +3,13 @@ import Directory from '../../components/directory/directory.component';
 
 import './homepage.style.scss';
 
-const HomePage = () => (
-	<div className='homepage'>
-		<Directory />
-	</div>
-);
+const HomePage = ({history}) => {
+	console.log(history)
+	return(
+		<div className='homepage'>
+			<Directory />
+		</div>
+	);
+}
 
 export default HomePage;
